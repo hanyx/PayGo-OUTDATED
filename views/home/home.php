20f2928a486bb28c4ed8bde19844df1e0bf220c4
Robert <robbiehotz13@gmail.com>
2015-07-12 17:50:05 -0500

    Fix features page layout Edit all text on homepages

--- views/home/home.php
+++ views/home/home.php
@@ -3,7 +3,7 @@ ___header();
 ?>
 <section class="hero">
     <h1 class="hero-h1">Sell Digital Products and Services</h1>
-    <h2 class="hero-h2">Accept Paypal, Bitcoin and Cryptocurrencies</h2>
+    <h2 class="hero-h2">Accept Paypal, Bitcoin and Altcoins</h2>
     <img class="macbook" src="/themes/home/img/macbook.png">
     <div class="hero-buttons">
         <a href="/features" class="hero-btn hero-btn-green">List Features</a>
@@ -13,23 +13,23 @@ ___header();
 </section>
 <section class="support">
     <h1 class="hero-h1">Customize Your Business</h1>
-    <h2 class="hero-h2">Multiple cryptocurrencies supported</h2>
+    <h2 class="hero-h2">Multiple Currencies Supported</h2>
     <div class="row">
         <div class="feature-list">
             <div class="col-md-3 feature-item ">
                 <img src="/themes/home/img/customize_1.png" height="75">
                 <h4 class="">Security</h4>
-                <p>Protect digital contents and your business from fraud.</p>
+                <p>Protect your business and digital items from fraud</p>
             </div>
             <div class="col-md-3 feature-item">
                 <img src="/themes/home/img/customize_2.png" height="75">
-                <h4 class="">Heading</h4>
-                <p>Unlimited products listing with up to 500 MB of upload.</p>
+                <h4 class="">Storage</h4>
+                <p>List unlimited products with up to 500 MB of storage.</p>
             </div>
             <div class="col-md-3 feature-item">
                 <img src="/themes/home/img/customize_3.png" height="75">
-                <h4 class="">Flexible</h4>
-                <p>Flexible integrations to support different types of businesses.</p>
+                <h4 class="">Flexibility</h4>
+                <p>Support many types of online businesses with flexible options.</p>
             </div>
         </div>
     </div>
@@ -37,22 +37,22 @@ ___header();
 
 <section class="checkout">
     <div class="container">
-        <h1 class="checkout-header">Simple checkouts for digital products</h1>
-        <h4 class="checkout-slogan">Keep your customers happy with instant digital products delivery</h4>
+        <h1 class="checkout-header">Simple Checkouts for Digital Products</h1>
+        <h4 class="checkout-slogan">Keep Your Customers Happy with Instant Digital Product Delivery</h4>
         <img src="/themes/home/img/checkout.png" style=" margin-left: auto;margin-right: auto; position: absolute; bottom: 0; left: 0; right: 0;">
         <div class="clearfix"></div>
     </div>
 </section>
 <section class="control-w-crypto">
-    <h1 class="control-header">Full control with cryptocurrency</h1>
-    <h4 class="control-slogan">Getting paid with bitcoin is easier than buying it</h4>
+    <h1 class="control-header">Take Control with Cryptocurreny Payments</h1>
+    <h4 class="control-slogan">Avoid Unneccessary Hassle and Stop Chargebacks</h4>
     <img src="/themes/home/img/btc_dollar.png" width="284">
 </section>
 
     <section class="register" style="">
         <div class="container register-container">
             <div style="text-align:center;">
-                <h1>Registration is easy</h1>
+                <h1>Registration is Easy</h1>
             </div>
             <form class="form-inline" method="post" action="/seller/register" style="text-align:center;">
                 <div class="form-group form-group-lg">
@@ -66,7 +66,7 @@ ___header();
                 </div>
             </form>
             <div class="text-center form-text" style="margin-top:15px;">
-                By signing up you agree to be bound by our <a href="#">Terms of Service</a> and our <a href="//www.iubenda.com/privacy-policy/954106" class="no-brand iubenda-nostyle iubenda-embed" title="Privacy Policy">Privacy Policy</a>
+                By signing up you agree to  our <a href="#">Terms of Service</a> and our <a href="//www.iubenda.com/privacy-policy/954106" class="no-brand iubenda-nostyle iubenda-embed" title="Privacy Policy">Privacy Policy</a>
             </div>
         </div>
         <div class="clearfix"></div>
