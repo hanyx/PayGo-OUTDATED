0fa6a4ac248397581d32989858b34dbf24507a0d
Robert <robbiehotz13@gmail.com>
2015-07-12 21:37:27 -0500

    Fix NN logo

--- views/home/footer.php
+++ views/home/footer.php
@@ -7,7 +7,7 @@ function ___footer($fixedfooter = false) {
             <div class="navbar footer-navigation navbar-collapse">
                 <div class="navbar-inner">
                     <ul class="nav">
-                        <li><a href="http://newnotefinancial.com/" class="footer-nnf"><img src="//uploads.webflow.com/5578e7f1be846a870957056a/5580f3dc5fd01fd44f5e3e99_Newnotefinancial-logo.jpg" width="178"></a>
+                        <li><a href="http://newnotefinancial.com/" class="footer-nnf"><img src="/themes/home/img/Newnotefinancial-logo.jpg" width="178"></a>
                         </li>
                         <li><a href="#">Terms and Conditions</a></li>
                         <li><a href="//www.iubenda.com/privacy-policy/954106" class="no-brand iubenda-nostyle iubenda-embed" title="Privacy Policy">Privacy Policy</a></li>
