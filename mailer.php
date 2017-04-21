5b0f71ba2785a9de1951f35fb89af08b8ce05b6e
Robert Hotz <robbiehotz13@gmail.com>
2017-04-21 18:02:31 -0500

    Delete mailer.php

--- mailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-//TODO: Move to dedicated admin panel
-//A very, very rough script for sending emails
-die();
-require_once('lib/functions.php');
-
-$mailer = new Mailer();
-
-$mailer->send('scrible97@gmail.com', 'Thanks for Purchasing LAN Accounts Lv30 Unverified<br>Here is what you bought: richistin:richistin10<br>', 'Your Purchase on PayIvy.com');
